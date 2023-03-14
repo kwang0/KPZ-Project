0de4a2274cf3e78c99e4767323e5ec59cd7a9cc2
kwang0 <kvnwang98@gmail.com>
2023-03-14 13:01:15 -0700

    mpo code written

--- heisenberg_ladder_mpo.py
+++ heisenberg_ladder_mpo.py
@@ -1,18 +1,18 @@
 from tenpy.models.xxz_chain import XXZChain
 from tenpy.networks.purification_mps import PurificationMPS
 from tenpy.algorithms.purification import PurificationTEBD, PurificationApplyMPO
-from tenpy.algorithms import tdvp
+from tenpy.algorithms.mpo_evolution import ExpMPOEvolution
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
-def imag_tebd(L=128, beta_max=3., dt=0.05, order=2, bc="finite"):
+def imag_tebd(L=128, beta_max=3., dt=0.05, chi_max=256, order=2, bc="finite"):
     model_params = dict(L=L, Jxx=1., Jz=1., hz=0)
-    M = XXZChain(model_params)
-    psi = PurificationMPS.from_infiniteT(M.lat.mps_sites(), bc=bc)
+    model = XXZChain(model_params)
+    psi = PurificationMPS.from_infiniteT(model.lat.mps_sites(), bc=bc)
     options = {
         'trunc_params': {
-            'chi_max': 256,
+            'chi_max': chi_max,
             'svd_min': 1.e-8
         },
         'order': order,
@@ -20,26 +20,43 @@ def imag_tebd(L=128, beta_max=3., dt=0.05, order=2, bc="finite"):
         'N_steps': 1
     }
     beta = 0.
-    eng = PurificationTEBD(psi, M, options)
+    eng = PurificationTEBD(psi, model, options)
     while beta < beta_max:
         beta += 2. * dt  # factor of 2:  |psi> ~= exp^{- dt H}, but rho = |psi><psi|
         eng.run_imaginary(dt)  # cool down by dt
     
+    return psi, model
+
+def real_mpo(psi, model, N_steps = 1000, dt = 0.1, chi_max = 256):
+    options = {
+        'trunc_params': {
+            'chi_max': chi_max,
+            'svd_min': 1.e-8
+        }
+    }
+
+    Us = [model.H_MPO.make_U(-d * 1.j * dt, 'II') for d in [0.5 + 0.5j, 0.5 - 0.5j]]
+    mpo_evolve = PurificationApplyMPO(psi, Us[0], options)
+
     psi2 = psi.copy()
     psi2.apply_local_op(0,'Sz') # Time-evolve Sz|psi> and |psi> separately
-    eng2 = PurificationTEBD(psi2, M, options)
+    mpo2_evolve = PurificationApplyMPO(psi2, Us[0], options)
 
     times = []
     corrs = []
-    for i in range(200):
-        eng.run_evolution(1,0.5)
-        eng2.run_evolution(1,0.5)
+    for i in range(N_steps):
+        for U in Us:
+            mpo_evolve.init_env(U)
+            mpo_evolve.run()
+
+            mpo2_evolve.init_env(U)
+            mpo2_evolve.run()
 
         psi3 = psi2.copy()
         psi3.apply_local_op(0,'Sz')
 
-        times.append(eng.evolved_time)
-        print(eng.evolved_time)
+        times.append(i * dt)
+        print(i * dt)
         corrs.append(np.abs(psi.overlap(psi3)))
 
     return times, corrs
@@ -48,7 +65,9 @@ if __name__ == "__main__":
     import logging
     logging.basicConfig(level=logging.INFO)
 
-    times, corrs = imag_tebd()
+    psi, model = imag_tebd(L=32)
+    times, corrs = real_mpo(psi, model, chi_max = 64)
+
 
     plt.loglog(times, corrs)
     plt.xlabel(r'$t$')

844107e98eaf7e45714a646fb90f28263357aa2a
kwang0 <kvnwang98@gmail.com>
2023-03-21 10:03:36 -0700

    fixed x=L/2 and auto-saved data during runs

--- heisenberg_ladder_mpo.py
+++ heisenberg_ladder_mpo.py
@@ -1,13 +1,14 @@
 from tenpy.models.xxz_chain import XXZChain
 from tenpy.networks.purification_mps import PurificationMPS
 from tenpy.algorithms.purification import PurificationTEBD, PurificationApplyMPO
-from tenpy.algorithms.mpo_evolution import ExpMPOEvolution
-import copy
+from tenpy.networks.mps import MPSEnvironment
 import numpy as np
 import matplotlib.pyplot as plt
+import h5py
 
+# Purification to finite temperature of beta_max (Schollwock 7.2)
 def imag_tebd(L=128, beta_max=3., dt=0.05, chi_max=256, order=2, bc="finite"):
-    model_params = dict(L=L, Jxx=1., Jz=1., hz=0)
+    model_params = dict(L=L, Jxx=1., Jz=1., hz=0, sort_charge=True)
     model = XXZChain(model_params)
     psi = PurificationMPS.from_infiniteT(model.lat.mps_sites(), bc=bc)
     options = {
@@ -27,7 +28,8 @@ def imag_tebd(L=128, beta_max=3., dt=0.05, chi_max=256, order=2, bc="finite"):
     
     return psi, model
 
-def real_mpo(psi, model, N_steps = 1000, dt = 0.1, chi_max = 256):
+# Real time evolution using MPO
+def real_mpo(psi, model, N_steps = 1000, dt = 0.1, chi_max = 256, beta = 3):
     options = {
         'trunc_params': {
             'chi_max': chi_max,
@@ -38,8 +40,9 @@ def real_mpo(psi, model, N_steps = 1000, dt = 0.1, chi_max = 256):
     Us = [model.H_MPO.make_U(-d * 1.j * dt, 'II') for d in [0.5 + 0.5j, 0.5 - 0.5j]]
     mpo_evolve = PurificationApplyMPO(psi, Us[0], options)
 
+    L = psi.L
     psi2 = psi.copy()
-    psi2.apply_local_op(0,'Sz') # Time-evolve Sz|psi> and |psi> separately
+    psi2.apply_local_op(L//2,'Sz') # Time-evolve Sz|psi> and |psi> separately
     mpo2_evolve = PurificationApplyMPO(psi2, Us[0], options)
 
     times = []
@@ -52,12 +55,16 @@ def real_mpo(psi, model, N_steps = 1000, dt = 0.1, chi_max = 256):
             mpo2_evolve.init_env(U)
             mpo2_evolve.run()
 
-        psi3 = psi2.copy()
-        psi3.apply_local_op(0,'Sz')
+        env = MPSEnvironment(psi, psi2)
 
         times.append(i * dt)
-        print(i * dt)
-        corrs.append(np.abs(psi.overlap(psi3)))
+        print('t =',i * dt)
+        corrs.append(np.abs(env.expectation_value('Sz', sites=[L//2])[0]))
+
+        # beta = 3
+        with h5py.File("tests/mpo_L{}_chi{}_beta{}_dt{}.h5".format(L, chi_max, beta, dt),'w') as F:
+            F['times'] = times
+            F['corrs'] = corrs
 
     return times, corrs
 
@@ -65,8 +72,9 @@ if __name__ == "__main__":
     import logging
     logging.basicConfig(level=logging.INFO)
 
-    psi, model = imag_tebd(L=32)
-    times, corrs = real_mpo(psi, model, chi_max = 64)
+    beta = 1
+    psi, model = imag_tebd(beta_max=beta, L=128)
+    times, corrs = real_mpo(psi, model, chi_max=256, N_steps=1000, dt=0.1, beta=beta)
 
 
     plt.loglog(times, corrs)
